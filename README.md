964a6563d3dcc054d95ad66a9e0097e8b0aea09b
Elie Rotenberg <elie@rotenberg.io>
2014-08-28 16:36:40 +0200

    Added IfMounted and fixed some critical bugs.

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 react-defer
 ===========
 Just a quick mixin to avoid headaches and `componentWillMount`/`componentWillUnmount` boilerplate when dealing with deferreds and intervals in components that get unmounted.
-
 Supports executing any deferred inline function or auto-defers a method. Also works with intervals.
+
+The enforced semantics are the following:
+"Execute the function if the component is still mounted by the time it kicks in. Otherwise, silently discard it altogether and clean up after yourself."
+
+In particular, if you chain deferrals, you may end up calling deferred callbacks after the component has unmounted (for exemple when a Promise of whatever is resolved).
+Wrap the callback in Defer.IfMounted and if this happens, it will silently fail.
 
 ### Examples
 
 ```jsx
-var DeferMixin = require("react-defer").Mixin;
-var Deferred = require("react-defer").Deferred;
+var Defer = require("react-defer");
 
 React.createClass({
 	mixins: [DeferMixin],
-	alwaysDeferThisMethod: Deferred(function() {
+	alwaysDeferThisMethod: Defer.Deferred(function() {
 		this.setState({
 			hello: "world",
 		});
 	}),
-	alwaysRAFThisMethod: DeferredAnimationFrame(function() {
+	alwaysRAFThisMethod: Defer.DeferredAnimationFrame(function() {
 		somethingThatAlwaysNeedsToBeDoneInNextAF();
 	}),
 	nonDeferredMethod: function() {
@@ -27,6 +31,9 @@ React.createClass({
 	},
 	someOtherMethod: function() {
 		this.defer(this.nonDeferredMethod)("some", "args");
+	},
+	someInterfacingMethod: function() {
+		this.giveMeSomthingReturningAPromise().then(Defer.IfMounted(this.nonDeferredMethod));
 	},
 }
 });
@@ -53,6 +60,9 @@ Uses [the `raf` npm package](https://www.npmjs.org/package/raf) `requestAnimatio
 #### `Mixin.intervalAnimationFrame(fn: function)`
 Similar to `interval` but instead ticks on each next animation frame.
 Uses [the `raf` npm package](https://www.npmjs.org/package/raf) `requestAnimationFrame` polyfill.
+
+#### `IfMounted(fn: function): function`
+Returns a function with the same this context that will silently fail if it is invoked after the component has unmounted.
 
 #### `Deferred(fn: function): function`
 Returns a function with the same `this` context that is deferred. Useful for defining methods that are always called deferred.
--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 var _ = require("lodash");
 var raf = require("raf");
+var assert = require("assert");
 
 var Mixin = {
-	_deferMixinTimeoutsLastIndex: null,
 	_deferMixinTimeouts: null,
-	_deferMixinIntervalsLastIndex: null,
 	_deferMixinIntervals: null,
-	_deferMixinAFTimeoutsLastIndex: null,
 	_deferMixinAFTimeouts: null,
-	_deferMixinAFIntervalsLastIndex: null,
 	_deferMixinAFIntervals: null,
-	componentWillMount: function componentWillMount() {
-		this._deferMixinIntervalsLastIndex = 0;
-		this._deferMixinTimeouts = {};
-		this._deferMixinIntervalsLastIndex = 0;
-		this._deferMixinIntervals = {};
-		this._deferMixinTimeoutsLastIndex = 0;
-		this._deferMixinAFTimeouts = {};
-		this._deferMixinIntervalsLastIndex = 0;
-		this._deferMixinAFIntervals = {};
-	},
+	_deferMixinHasUnmounted: null,
 	deferAnimationFrame: function deferToNextAnimationFrame(fn) {
 		var _this = this;
-		return function() {
-			var k = this._deferMixinTimeoutsLastIndex++;
+		return IfMounted(function() {
+			var k = _.uniqueId("deferAnimationFrame");
 			var args = arguments;
 			var t = raf(function() {
 				delete _this._deferMixinAFTimeouts[k];
@@ -36,73 +24,103 @@ var Mixin = {
 				},				
 			};
 			return r;
-		};
+		});
 	},
 	intervalAnimationFrame: function intervalAnimationFrame(fn) {
 		var _this = this;
-		return function() {
-			var k = this._deferMixinAFIntervalsLastIndex++;
+		return IfMounted(function() {
+			if(this._deferMixinHasUnmounted) {
+				return;
+			}
+			var k = _.uniqueId("intervalAnimationFrame");
 			var args = arguments;
 			var i = raf(function z() {
 				i = raf(z);
 				fn.apply(_this, args);
 			});
-			var r = this._deferMixinAFIntervals[k] = {
+			var r = _this._deferMixinAFIntervals[k] = {
 				clear: function clear() {
 					i.cancel();
 					delete _this._deferMixinIntervals[k];
 				}
 			};
 			return r;
-		};
+		});
 	},
 	timeout: function timeout(fn, delay) {
 		var _this = this;
-		return function() {
-			var k = this._deferMixinTimeoutsLastIndex++;
+		return IfMounted(function() {
+			if(this._deferMixinHasUnmounted) {
+				return;
+			}
+			var k = _.uniqueId("timeout");
 			var args = arguments;
 			var t = setTimeout(function() {
 				delete _this._deferMixinTimeouts[k];
 				fn.apply(_this, args);
 			}, delay);
-			var r = this._deferMixinTimeouts[k] = {
+			var r = _this._deferMixinTimeouts[k] = {
 				clear: function clear() {
 					clearTimeout(t);
 					delete _this._deferMixinTimeouts[k];
 				},				
 			};
 			return r;
-		};
+		});
 	},
 	defer: function defer(fn) {
 		return this.timeout(fn, 1);
 	},
 	interval: function interval(fn, period) {
 		var _this = this;
-		return function() {
-			var k = this._deferMixinIntervalsLastIndex++;
+		return IfMounted(function() {
+			if(this._deferMixinHasUnmounted) {
+				return;
+			}
+			k = _.uniqueId("interval");
 			var args = arguments;
 			var i = setInterval(function() {
 				fn.apply(_this, args);
 			}, period);
-			var r = this._deferMixinIntervals[k] = {
+			var r = _this._deferMixinIntervals[k] = {
 				clear: function clear() {
 					clearInterval(i);
 					delete _this._deferMixinIntervals[k];
 				},
 			};
 			return r;
-		};
+		});
+	},
+	componentWillMount: function componentWillMount() {
+		this._deferMixinTimeouts = {};
+		this._deferMixinIntervals = {};
+		this._deferMixinAFTimeouts = {};
+		this._deferMixinAFIntervals = {};
+		this._deferMixinHasUnmounted = false;
 	},
 	componentWillUnmount: function componentWillUnmount() {
+		this._deferMixinHasUnmounted = true;
 		_.each(this._deferMixinTimeouts, function(r) { r.clear(); });
 		_.each(this._deferMixinIntervals, function(r) { r.clear(); });
+		_.each(this._deferMixinAFTimeouts, function(r) { r.clear(); });
+		_.each(this._deferMixinAFIntervals, function(r) { r.clear(); });
 	},
 };
 
 var Deferred = function Deferred(fn) {
 	return function() {
 		return this.defer(fn).apply(this, arguments);
+	};
+};
+
+var IfMounted = function IfMounted(fn) {
+	return function() {
+		if(this._deferMixinHasUnmounted) {
+			return;
+		}
+		else {
+			return fn.apply(this, arguments);
+		}
 	};
 };
 
@@ -114,6 +132,7 @@ var DeferredAnimationFrame = function DeferredAnimationFrame(fn) {
 
 module.exports = {
 	Mixin: Mixin,
+	IfMounted: IfMounted,
 	Deferred: Deferred,
 	DeferredAnimationFrame: DeferredAnimationFrame,
 };
